34e77816cbf884b4ce3c7b4b27f0074df6fc8115
Sonic_SV <sonicsv@mail.ru>
2021-01-26 23:38:53 +0500

    add slider and tabs

--- js/script.js
+++ js/script.js
@@ -3,7 +3,19 @@ import 'jquery';
 import 'nodelist-foreach-polyfill';
 
 import tabs from './modules/tabs';
+import slider from './modules/slider';
 
 window.addEventListener('DOMContentLoaded', () => {
+
     tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    slider({
+        container: '.offer__slider',
+        slide: '.offer__slide',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner'
+    });
 });
